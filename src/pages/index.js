f64a9795594de31128bfe517547718ad10c6aa13
Wung <wungmathing@gmail.com>
2017-11-04 20:59:55 +0530

    First commit

---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import React from "react"
+import Link from "gatsby-link"
+
+export default () => (
+  <div>
+    <h1> Hello New Site! </h1>
+    <Link to="/page-2/">Link</Link>
+  </div>
+)
---
+++ src/pages/page-2.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+export default () => (
+  <div>
+    <h1> Hello Page 2! </h1>
+  </div>
+)
